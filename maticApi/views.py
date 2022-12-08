3913ed2beb3dd7d2698293bf7d1d1817b6df74c1
TayyabImran009 <tayyabimran886@gmail.com>
2022-12-08 04:16:53 +0000

    API changes

--- maticApi/views.py
+++ maticApi/views.py
@@ -1,9 +1,54 @@
 from django.shortcuts import render
 
+import requests
+import json
 # Create your views here.
 
-def home(request):
-    context = {
-        'tab_title':'Home',
+def call_copymatic(content,model,tone,creativity,language,audience,keywords):
+    payload = json.dumps({
+            "model": model,
+            "tone": tone,
+            "creativity":creativity,
+            "topic":content,
+            "language": language,
+            "audience": audience,
+            "keywords": keywords,
+        })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer dff15f467e4ddc7801d670738',
     }
-    return render(request,'maticApi/home.html')
+    response = requests.request("POST", 'https://api.copymatic.ai', headers=headers, data=payload)
+
+    print(response.json(),"************")
+    return response.json()
+
+def home(request):
+
+    if request.method == 'POST':
+        print(request.POST['content'],"Content")
+        print(request.POST['model'],"Model")
+        print(request.POST['tone'],"Tone")
+        print(request.POST['creativity'],"Creativity")
+        print(request.POST['language'],"Language")
+        print(request.POST['audience'],"Audience")
+        print(request.POST['keywords'],"Keywords")
+
+        response = call_copymatic(
+                        request.POST['content'],
+                        request.POST['model'],
+                        request.POST['tone'],
+                        request.POST['creativity'],
+                        request.POST['language'],
+                        request.POST['audience'],
+                        request.POST['keywords']
+                    )
+        context = {
+            'tab_title':'Home',
+            'response':response['ideas'].values()
+        }
+    else:
+        context = {
+            'tab_title':'Home',
+        }
+    return render(request,'maticApi/home.html',context)
